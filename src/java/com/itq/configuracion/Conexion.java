962ee04242f61840075047ea289821df1a0b41af
Paul Alvarez <paulalvarezcorral@gmail.com>
2022-03-15 22:08:46 -0500

    Conexion

--- src/java/com/itq/configuracion/Conexion.java
+++ src/java/com/itq/configuracion/Conexion.java
@@ -2,6 +2,7 @@ package com.itq.configuracion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -9,23 +10,35 @@ import java.sql.DriverManager;
  */
 public class Conexion {
     
-    public static Connection conn;
-    private static final String USER = "root";
-    private static final String CLAVE = "Palvarez11";
-    private static final String URL = "jdbc:mysql://localhost:3306/bdd_tienda?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
-    private static final String CLASS_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public Connection conn = null;
+    private final String USER = "root";
+    private final String CLAVE = "Palvarez11";
+    private final String URL = "jdbc:mysql://localhost:3306/bdd_tienda?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+    private final String CLASS_DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    public static Connection getConnetion() {
+    protected Connection getConnetion() throws SQLException {
         try {
-            //VOID --> SIN RETORNO
-            //TIENE UN TIPO DE DATO A DEVOLVER y RETURN --A CON RETORNO
             Class.forName(CLASS_DRIVER).newInstance();
-            conn = DriverManager.getConnection(URL, USER, CLAVE);
         } catch (Exception ex) {
-            System.out.println("Error al abrir la conexión ");
-            ex.printStackTrace();
+            throw new SQLException("Error al abrir la conexión ");
         }
+        
+        if (conn == null || conn.isClosed()) {
+            conn = DriverManager.getConnection(URL, USER, CLAVE);
+            System.out.println("Conexion exitosa");
+        }
+        
         return conn;
+    }
+    
+    protected void cerrar() {
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Exception ignored) {
+            }
+        }
     }
     
 }
