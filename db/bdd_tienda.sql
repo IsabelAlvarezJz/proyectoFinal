dff30278250d4f3b85cb720db13c72f8029625d2
Paul Alvarez <paulalvarezcorral@gmail.com>
2022-03-18 08:28:09 -0500

    Actualizo script bdd_tienda.sql

--- db/bdd_tienda.sql
+++ db/bdd_tienda.sql
@@ -3,7 +3,7 @@ CREATE TABLE `cliente` (
   `nombres` varchar(255) DEFAULT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
   `perfil_tipo` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -108,5 +108,92 @@ ALTER TABLE `detalle_compras`
 -- ALTER TABLE `cliente`
 --  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`perfil_tipo`) REFERENCES `perfil` (`idperfil`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
+-- PERFILES
 INSERT INTO `bdd_tienda`.`perfil` (`idperfil`, `tipo`, `descripcion`) VALUES ('1', 'Administrador', 'Perfil para Administrador');
 INSERT INTO `bdd_tienda`.`perfil` (`idperfil`, `tipo`, `descripcion`) VALUES ('2', 'Cliente', 'Perfil para Cliente');
+INSERT INTO `bdd_tienda`.`perfil` (`idperfil`, `tipo`, `descripcion`) VALUES ('3', 'Proveedor', 'Perfil para Proveedores');
+
+-- USUARIOS
+INSERT INTO `bdd_tienda`.`cliente` (`cedula`, `nombres`, `direccion`, `email`, `password`, `perfil_tipo`) 
+VALUES ('1231231230', 'Usuario Administrador', 'Quito', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '1');
+INSERT INTO `bdd_tienda`.`cliente` (`cedula`, `nombres`, `direccion`, `email`, `password`, `perfil_tipo`) 
+VALUES ('1234123400', 'Usuario Cliente', 'Quito', 'cliente@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2');
+
+#Clave Admin:      123456
+#Clave Cliente:    123456
+
+-- COMPRAS
+INSERT INTO `bdd_tienda`.`compras` (`idCompras`, `idPago`, `cedula`, `fechaCompra`, `monto`, `estado`) 
+VALUES (1, '1', '1234123400', '2022-03-18', '2200', 'PROCESADO');
+INSERT INTO `bdd_tienda`.`compras` (`idCompras`, `idPago`, `cedula`, `fechaCompra`, `monto`, `estado`) 
+VALUES (2, '1', '1234123400', '2022-03-18', '2200', 'PROCESADO');
+INSERT INTO `bdd_tienda`.`compras` (`idCompras`, `idPago`, `cedula`, `fechaCompra`, `monto`, `estado`) 
+VALUES (3, '1', '1234123400', '2022-03-18', '2200', 'PROCESADO');
+INSERT INTO `bdd_tienda`.`compras` (`idCompras`, `idPago`, `cedula`, `fechaCompra`, `monto`, `estado`) 
+VALUES (4, '1', '1234123400', '2022-03-18', '2200', 'PROCESADO');
+INSERT INTO `bdd_tienda`.`compras` (`idCompras`, `idPago`, `cedula`, `fechaCompra`, `monto`, `estado`) 
+VALUES (5, '1', '1234123400', '2022-03-18', '2200', 'PROCESADO');
+
+
+-- PRODUCTOS
+INSERT INTO `bdd_tienda`.`producto` (`codProducto`, `nombres`, `descripcion`, `precio`, `stock`, `ruta`) 
+VALUES ('001', 'Producto 1', 'Descripción producto 1', '35', '100', 'http://localhost/img/producto1.jpg');
+INSERT INTO `bdd_tienda`.`producto` (`codProducto`, `nombres`, `descripcion`, `precio`, `stock`, `ruta`) 
+VALUES ('002', 'Producto 2', 'Descripción producto 2', '45', '60', 'http://localhost/img/producto2.jpg');
+INSERT INTO `bdd_tienda`.`producto` (`codProducto`, `nombres`, `descripcion`, `precio`, `stock`, `ruta`) 
+VALUES ('003', 'Producto 3', 'Descripción producto 3', '10', '200', 'http://localhost/img/producto3.jpg');
+INSERT INTO `bdd_tienda`.`producto` (`codProducto`, `nombres`, `descripcion`, `precio`, `stock`, `ruta`) 
+VALUES ('004', 'Producto 4', 'Descripción producto 4', '80', '50', 'http://localhost/img/producto4.jpg');
+INSERT INTO `bdd_tienda`.`producto` (`codProducto`, `nombres`, `descripcion`, `precio`, `stock`, `ruta`) 
+VALUES ('005', 'Producto 5', 'Descripción producto 5', '14', '30', 'http://localhost/img/producto5.jpg');
+
+
+-- DETALLE COMPRA
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (1, 1, '001', '2', '35');
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (2, 1, '002', '1', '45');
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (3, 2, '001', '1', '45');
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (4, 2, '004', '1', '80');
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (5, 3, '005', '10', '14');
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (6, 4, '002', '10', '45');
+INSERT INTO `bdd_tienda`.`detalle_compras` (`idDetalle`, `idCompras`, `codProducto`, `cantidad`, `precioCompra`) 
+VALUES (7, 5, '001', '10', '35');
+
+
+-- PAGO
+INSERT INTO `bdd_tienda`.`pago` (`idPago`, `monto`) VALUES (1, '80');
+INSERT INTO `bdd_tienda`.`pago` (`idPago`, `monto`) VALUES (2, '125');
+INSERT INTO `bdd_tienda`.`pago` (`idPago`, `monto`) VALUES (3, '14');
+INSERT INTO `bdd_tienda`.`pago` (`idPago`, `monto`) VALUES (4, '45');
+INSERT INTO `bdd_tienda`.`pago` (`idPago`, `monto`) VALUES (5, '35');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
